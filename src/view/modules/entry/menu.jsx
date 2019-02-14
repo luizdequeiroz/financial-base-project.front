97ce535e82fb1e70e7e6a5cc4adbff99ab4c46e2
Luiz Viana de Queiroz Neto <luizdequeiroz@outlook.com.br>
2019-02-14 19:03:47 -0300

    adicionando prop key recebendo indeces do map para renderização de menus

--- src/view/modules/entry/menu.jsx
+++ src/view/modules/entry/menu.jsx
@@ -10,8 +10,8 @@ class Menu extends Component {
             <fieldset>
                 <legend>Menu</legend>
                 <div className="flex-menu">
-                    {menus.map(menu => (
-                        <div className="flex-menu-item">
+                    {menus.map((menu, i) => (
+                        <div className="flex-menu-item" key={i}>
                             <div className={`panel panel-${menu.type}`} onClick={() => window.location.hash = menu.route}>
                                 <div className="panel-heading"><i className={`fa fa-${menu.icon} fa-5x`} /></div>
                                 <div className="panel-body">{menu.text}</div>
